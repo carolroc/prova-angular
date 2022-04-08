9b08cb23fd829203096648d0c81ca6b086ae1b9d
carolroc <carolroc02@gmail.com>
2022-04-08 20:39:41 -0300

    3 files modified

--- src/app/ana-disciplinas-service.service.ts
+++ src/app/ana-disciplinas-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-interface Disciplinas {
+interface AnaDisciplinasService {
   cod: string;
   nome: string;
   diaDaSemana: string;
@@ -10,28 +10,24 @@ interface Disciplinas {
 
 @Injectable()
 export class AnaDisciplinasServiceService {
-  list: string[] = [];
-  constructor() {
-    //this.list.push('ISW014A','Projeto de Encontrabilidade' ,'Segunda' ,'19h' ,'Teoria Geral. Marcação para encontrabilidade. Microformatos. Conteúdo. Rede Social. Estratégias Server-Side. Como evitar armadilhas de encontrabilidade. Pesquisas. Análises' );
-
-    //this.list.push('ISD002A','Arquitetura Orientada a Serviços' ,'Quarta' ,'19h' ,'Construção de aplicações por meio da utilização de arquitetura orientada a serviços – SOA (Service-Oriented Architecture) e das principais tecnologias Web Services' );
-
-    //this.list.push('ITE004A','Tópicos Especiais em Sistemas para Internet II' ,'Sexta' ,'19h' ,'Recursos e aplicações da tecnologia escolhida' );
-
-    //this.list.push('TTG401A','Projeto de Trabalho de Graduação em Sistemas para Internet I' ,'Sábado' ,'11:30' ,'Conceito de portfólio no âmbito da educação superior. Planejamento e organização de portfólios' );
-  }
+  list: Array<AnaDisciplinasService> = [];
+  constructor() {}
 
   add(
-    codigo: string,
+    cod: string,
     nome: string,
     diaDaSemana: string,
     horario: string,
     ementa: string
   ) {
-    this.list.push(codigo, nome, diaDaSemana, horario, ementa);
+    this.list.push({cod, nome, diaDaSemana, horario, ementa});
   }
 
   remove(index: number) {
     this.list.splice(index, 1);
+  }
+
+  getlist(){
+    return this.list;
   }
 }
--- src/app/ana-lista-component/ana-lista-component.component.ts
+++ src/app/ana-lista-component/ana-lista-component.component.ts
@@ -4,13 +4,14 @@ import { AnaDisciplinasServiceService } from '../ana-disciplinas-service.service
 @Component({
   selector: 'app-ana-lista-component',
   templateUrl: './ana-lista-component.component.html',
-  styleUrls: ['./ana-lista-component.component.css']
+  styleUrls: ['./ana-lista-component.component.css'],
 })
 export class AnaListaComponentComponent implements OnInit {
-
-  constructor(public disciplina: AnaDisciplinasServiceService) { }
-
-  ngOnInit() {
+  list: string[] = [];
+  constructor(public disciplina: AnaDisciplinasServiceService) {
   }
 
+  ngOnInit() {}
+
+  
 }
