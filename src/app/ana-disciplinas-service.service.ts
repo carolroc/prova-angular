442dcc8b328392a65a1b89242b08bbdfdf1f4abd
carolroc <carolroc02@gmail.com>
2022-04-08 20:04:39 -0300

    1 file modified, 1 added

--- src/app/ana-disciplinas-service.service.ts
+++ src/app/ana-disciplinas-service.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AnaDisciplinasServiceService {
-  list:string[] = [];
-  constructor() { }
+  list: string[] = [];
+  constructor() {}
 
-  add(codigo: string, nome: string, diaDaSemana: string, horario: string){
-    this.list.push(codigo,nome,diaDaSemana,horario);
+  add(
+    codigo: string,
+    nome: string,
+    diaDaSemana: string,
+    horario: string,
+    ementa: string
+  ) {
+    this.list.push(codigo, nome, diaDaSemana, horario, ementa);
   }
 
-  remove(index:number){
-    this.list.splice(index,1);
+  remove(index: number) {
+    this.list.splice(index, 1);
   }
 }
