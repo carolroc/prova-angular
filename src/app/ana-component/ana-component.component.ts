12ff412c2a0bf9b5863f2ef0a2ee9d4f0c310bd8
carolroc <carolroc02@gmail.com>
2022-04-08 19:42:22 -0300

    3 files added

---
+++ src/app/ana-component/ana-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ana-component',
+  templateUrl: './ana-component.component.html',
+  styleUrls: ['./ana-component.component.css']
+})
+export class AnaComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
